f347af3f2c0313d7ebdc109c7f0bacb9040933b0
Jhonnatas Ribeiro <jhonnatasribeiro01@gmail.com>
2024-06-14 05:18:44 -0300

    slide horizontal

--- estudo001/usingScrollView/App.js
+++ estudo001/usingScrollView/App.js
@@ -4,8 +4,8 @@ import Card from './src/components/Card';
 export default function App() {
   return (
     <View style={styles.container}>
-      <ScrollView style={styles.carrossel}>
-        <View style={styles.carrossel}>
+      <ScrollView>
+      <ScrollView horizontal style={styles.carrossel}>
         <Card nome="Neymar" url="https://i0.statig.com.br/bancodeimagens/imgalta/6d/2n/qs/6d2nqshw9lodueisju2k2bwyl.jpg" />
         <StatusBar style="auto" />
         <Card nome="Suarez" url="https://i.pinimg.com/550x/02/c4/e9/02c4e96ff0f3aea80a938fffe931c05b.jpg" />
@@ -13,7 +13,19 @@ export default function App() {
         <Card nome="Messi" url="https://neofeed.com.br/wp-content/uploads/2021/08/messi-barcelona-1200x900.jpg" />
         <Card nome="Mbappe" url="https://c4.wallpaperflare.com/wallpaper/920/140/899/soccer-kylian-mbappe-french-world-cup-2018-wallpaper-preview.jpg" />
         <Card nome="Cristiano Ronaldo" url="https://conteudo.imguol.com.br/c/esporte/62/2022/10/26/cristiano-ronaldo-em-acao-com-a-camisa-da-selecao-de-portugal-1666822687434_v2_1x1.jpg" />
-        </View>
+      </ScrollView>
+      <View>
+        <Text>Teste</Text>
+        <ScrollView horizontal style={styles.carrossel}>
+          <Card nome="Neymar" url="https://i0.statig.com.br/bancodeimagens/imgalta/6d/2n/qs/6d2nqshw9lodueisju2k2bwyl.jpg" />
+          <StatusBar style="auto" />
+          <Card nome="Suarez" url="https://i.pinimg.com/550x/02/c4/e9/02c4e96ff0f3aea80a938fffe931c05b.jpg" />
+          <StatusBar style="auto" />
+          <Card nome="Messi" url="https://neofeed.com.br/wp-content/uploads/2021/08/messi-barcelona-1200x900.jpg" />
+          <Card nome="Mbappe" url="https://c4.wallpaperflare.com/wallpaper/920/140/899/soccer-kylian-mbappe-french-world-cup-2018-wallpaper-preview.jpg" />
+          <Card nome="Cristiano Ronaldo" url="https://conteudo.imguol.com.br/c/esporte/62/2022/10/26/cristiano-ronaldo-em-acao-com-a-camisa-da-selecao-de-portugal-1666822687434_v2_1x1.jpg" />
+        </ScrollView>
+      </View>
       </ScrollView>
     </View>
   );
@@ -25,8 +37,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  carrossel: {
-    display: 'block'
-  },
+  }
 });
--- estudo001/usingScrollView/src/components/Card/index.js
+++ estudo001/usingScrollView/src/components/Card/index.js
@@ -17,14 +17,10 @@ function Card({ url, nome }) {
 const styles = {
     card: {
         backgroundColor: '#fff',
-        margin: 10,
-        padding: 10,
-        borderRadius: 5,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 1 },
-        shadowOpacity: 0.8,
-        shadowRadius: 2,
-        elevation: 5,
+        borderWidth: 1,
+        height: 300,
+        borderColor: '#ddd',
+      
     },
     title: {
         fontSize: 18,
